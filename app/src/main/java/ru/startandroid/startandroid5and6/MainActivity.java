0e9145a999f57fa99b6ed3b136dbab6aa765a016
AntonSnezhkin <92733361+AntonSnezhkin@users.noreply.github.com>
2021-10-28 22:38:28 +0400

    Урок ворой

--- app/src/main/java/ru/startandroid/startandroid5and6/MainActivity.java
+++ app/src/main/java/ru/startandroid/startandroid5and6/MainActivity.java
@@ -2,23 +2,73 @@ package ru.startandroid.startandroid5and6;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
-        Button btnSecret = findViewById(R.id.btnSecret);
-        ConstraintLayout llSecret = findViewById(R.id.llSecret);
+        Button btnSecret, button1, button2, button3;
 
-        textView.setOnClickListener(view -> llSecret.setVisibility(View.VISIBLE));
-        btnSecret.setOnClickListener(view -> llSecret.setVisibility(View.INVISIBLE));
+        TextView textView = findViewById(R.id.textView);
+        textView.setOnClickListener(this);
+
+        btnSecret = findViewById(R.id.btnSecret);
+        btnSecret.setOnClickListener(this);
+
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        ConstraintLayout llSecret;
+        llSecret = (ConstraintLayout) findViewById(R.id.llSecret);
+        switch (view.getId()) {
+            case R.id.textView:
+                llSecret.setVisibility(View.VISIBLE);
+                break;
+            case R.id.btnSecret:
+                llSecret.setVisibility(View.INVISIBLE);
+                break;
+            case R.id.button1:
+                Toast.makeText(this, getResources().getString(R.string.toast), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Snackbar.make(view, getResources().getString(R.string.snackbar), Snackbar.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(getResources().getString(R.string.titleName))
+                        .setMessage(getResources().getString(R.string.alertMassage))
+                        .setCancelable(false)
+                        .setPositiveButton(getResources().getString(R.string.alertPositiveButton), (dialogInterface, i) -> {
+
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+                break;
+        }
+
     }
 }
