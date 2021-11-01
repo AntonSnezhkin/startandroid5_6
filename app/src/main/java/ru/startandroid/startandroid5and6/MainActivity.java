629c0b3ca2ac5b84dced9a3dd100b36412b4fd58
AntonSnezhkin <92733361+AntonSnezhkin@users.noreply.github.com>
2021-11-01 11:01:13 +0400

    Убрал ещё switch

--- app/src/main/java/ru/startandroid/startandroid5and6/MainActivity.java
+++ app/src/main/java/ru/startandroid/startandroid5and6/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,79 +19,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         Button btnSecret, button1, button2, button3, button4, button5;
-
-        TextView textView = findViewById(R.id.textView);
-        textView.setOnClickListener(this);
-
-        btnSecret = findViewById(R.id.btnSecret);
-        btnSecret.setOnClickListener(this);
-
-        button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(this);
-
-        button2 = findViewById(R.id.button2);
-        button2.setOnClickListener(this);
-
-        button3 = findViewById(R.id.button3);
-        button3.setOnClickListener(this);
-
-        button4 = findViewById(R.id.button4);
-        button4.setOnTouchListener(this);
-
-        button5 = findViewById(R.id.button5);
-        button5.setOnTouchListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
         ConstraintLayout llSecret;
         llSecret = findViewById(R.id.llSecret);
-        switch (view.getId()) {
-            case R.id.textView:
-                llSecret.setVisibility(View.VISIBLE);
-                break;
-            case R.id.btnSecret:
-                llSecret.setVisibility(View.INVISIBLE);
-                break;
-            case R.id.button1:
-                Toast.makeText(this, getResources().getString(R.string.toast), Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button2:
-                Snackbar.make(view, getResources().getString(R.string.snackbar), Snackbar.LENGTH_SHORT).show();
-                break;
-            case R.id.button3:
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle(getResources().getString(R.string.titleName))
-                        .setMessage(getResources().getString(R.string.alertMassage))
-                        .setCancelable(false)
-                        .setPositiveButton(getResources().getString(R.string.alertPositiveButton), (dialogInterface, i) -> {
-                        return;
-                        });
-                AlertDialog alert = builder.create();
-                alert.show();
-                break;
-            case R.id.button4:
-                Toast.makeText(this, getResources().getString(R.string.toast_btnTouchDown), Toast.LENGTH_SHORT).show();
-                break;
-        }
-    }
 
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        switch (view.getId()) {
-            case (R.id.button4):
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    Toast.makeText(this, getResources().getString(R.string.toast_btnTouchDown), Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                break;
-            case (R.id.button5):
-                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    Toast.makeText(this, getResources().getString(R.string.toast_btnTouchUp), Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                break;
+        TextView textView = findViewById(R.id.textView);
+        textView.setOnClickListener(view -> llSecret.setVisibility(View.VISIBLE));
+
+        btnSecret = findViewById(R.id.btnSecret);
+        btnSecret.setOnClickListener(view -> llSecret.setVisibility(View.INVISIBLE));
+
+
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(view -> Toast.makeText(this, getResources().getString(R.string.toast), Toast.LENGTH_SHORT).show());
+
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(view -> Snackbar.make(view, getResources().getString(R.string.snackbar), Snackbar.LENGTH_SHORT).show());
+
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(view -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                 builder.setTitle(getResources().getString(R.string.titleName))
+                .setMessage(getResources().getString(R.string.alertMassage))
+                .setCancelable(false)
+                .setPositiveButton(getResources().getString(R.string.alertPositiveButton), (dialogInterface, i) -> {
+                return;
+                });
+        AlertDialog alert = builder.create();
+        alert.show();});
+
+        button4 = findViewById(R.id.button4);
+        button4.setOnTouchListener((view, motionEvent) -> {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            Toast.makeText(this, getResources().getString(R.string.toast_btnTouchDown), Toast.LENGTH_SHORT).show();
+            return true;
+        }
+            return false;
+        });
+
+        button5 = findViewById(R.id.button5);
+        button5.setOnTouchListener((view, motionEvent) -> {
+        if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+            Toast.makeText(this, getResources().getString(R.string.toast_btnTouchUp), Toast.LENGTH_SHORT).show();
+            return true;
         }
         return false;
+        });
+
+
+
     }
 }
