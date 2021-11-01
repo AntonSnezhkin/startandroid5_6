c7ae6aea5531d8616a410a0cf8c37f5d56e6996c
AntonSnezhkin <92733361+AntonSnezhkin@users.noreply.github.com>
2021-11-01 10:13:53 +0400

    Убрал ещё пробелы

--- app/src/main/java/ru/startandroid/startandroid5and6/MainActivity.java
+++ app/src/main/java/ru/startandroid/startandroid5and6/MainActivity.java
@@ -2,7 +2,6 @@ package ru.startandroid.startandroid5and6;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -10,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
@@ -42,9 +40,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         button5 = findViewById(R.id.button5);
         button5.setOnTouchListener(this);
-
-
-
     }
 
     @Override
